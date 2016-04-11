91fe6ef2201f63cac1b2f37dbd182c72a2641ffa
Fabricio Nascimento <fabriciosn@gmail.com>
2016-04-11 15:24:38 -0300

    Small bug fixes

--- board/server/methods/players.js
+++ board/server/methods/players.js
@@ -6,10 +6,10 @@ import Players from '/imports/api/collections/players.js'
 import Transactions from '/imports/api/collections/transactions.js'
 
 Meteor.methods({
-  'player.add_points': (name, type) => {
+  'player.add_points': (name, type, amount) => {
     let user = Meteor.user();
     if (user){
-      Transactions.insert({owner: user.profile.name, type: type, created_at: moment(), player: name, points: 1});
+      Transactions.insert({owner: user.profile.name, type: type, created_at: moment(), player: name, points: amount});
       let updateField = {};
       updateField[type] = 1;
       Players.update({name}, {$inc: updateField });
