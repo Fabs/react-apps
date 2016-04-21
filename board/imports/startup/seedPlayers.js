ccc50ad676f71b784cd3656d1e9e78116d8c3c12
Fabricio Nascimento <fabriciosn@gmail.com>
2016-04-21 06:11:41 -0300

    Updates Points as during Tiradentes

--- board/imports/startup/seedPlayers.js
+++ board/imports/startup/seedPlayers.js
@@ -2,16 +2,17 @@ import Players from '/imports/api/collections/players.js';
 import moment from 'moment';
 
 const initialPlayers = [
-  {name: 'barbara', legacy_points: 2-1},
-  {name: 'felipe', legacy_points: 10+17},
-  {name: 'fernando', legacy_points: 13-1},
-  {name: 'fabs', legacy_points: 16+1},
-  {name: 'jander', legacy_points: 12+6},
-  {name: 'acarajé', legacy_points: 17+2},
-  {name: 'thamila', legacy_points: 9-1},
-  {name: 'william', legacy_points: 18+4},
-  {name: 'lenise', legacy_points: 2+1},
-  {name: 'edivaldo', legacy_points: 6+1},
+  {name: 'barbara', legacy_points: 2-2},
+  {name: 'felipe', legacy_points: 10+17-1+2},
+  {name: 'fernando', legacy_points: 13},
+  {name: 'fabs', legacy_points: 16+1-1},
+  {name: 'jander', legacy_points: 12+6-1},
+  {name: 'acarajé', legacy_points: 17+2-1},
+  {name: 'thamila', legacy_points: 9-2},
+  {name: 'william', legacy_points: 18+4-1},
+  {name: 'lenise', legacy_points: 2+1-1},
+  {name: 'edivaldo', legacy_points: 6+1-1},
+  {name: 'dai', legacy_points: 1},
 ];
 
 const seedPlayers = function(){
@@ -21,12 +22,6 @@ const seedPlayers = function(){
       Players.insert(Object.assign(initialPlayers[i],{jokes: 0, coffee: 0, bread:0}));
     }
   }
-
-  //Meteor.call('player.add_points', 'jander', 'coffee', 3)
-  //Meteor.call('player.add_points', 'jander', 'joke', 2)
-  //Meteor.call('player.add_points', 'acarajé', 'coffee', 2)
-  //Meteor.call('player.add_points', 'will', 'coffee', 1)
-  //Meteor.call('player.add_points', 'will', 'joke', 1)
 }
 
 export { seedPlayers };
