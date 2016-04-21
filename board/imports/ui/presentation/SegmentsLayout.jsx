71f37a2abbb5c0bc88aee873592f035bac45ef8b
Fabricio Nascimento <fabriciosn@gmail.com>
2016-04-21 18:05:24 -0300

    Add Two Column Layout

--- board/imports/ui/presentation/SegmentsLayout.jsx
+++ board/imports/ui/presentation/SegmentsLayout.jsx
@@ -8,26 +8,33 @@ export default class SegmentsLayout extends React.Component {
   render() {
     console.debug('RENDER',this);
     return (
-      <div className="ui stackable container">
-        <div className="ui menu header">
-          <span className="ui uix greyAccent item">
-            <i className="ui icon coffee"/> + <i className="ui icon heart"/> = <i className="ui icon smile"/>
-          </span>
-          <LoginBarContainer className="ui item right floated"/>
+      <div>
+        <div className="column row">
+          <div className="ui menu header">
+            <span className="ui uix greyAccent item">
+              <i className="ui icon coffee"/> + <i className="ui icon heart"/> = <i className="ui icon smile"/>
+            </span>
+            <LoginBarContainer className="ui item right floated"/>
+          </div>
         </div>
-
-        <h2 className="ui dividing header">Moderação</h2>
-        <ModerationContainer className='ui' />
-
-        <h2 className="ui dividing header">Pontos</h2>
-        <ScoreContainer className='ui' />
-
-        <h2 className="ui dividing header">Auditoria</h2>
-        <TransactionsContainer className='ui' />
-
-        <h2 className="ui dividing header">Links</h2>
-        <a href="https://loja.reativo.com/" target="_blank">Acesso a loja</a>
         <br/>
+        <div className="ui two column stackable grid">
+          <div className="column ten wide">
+            <h2 className="ui dividing header">Pontos</h2>
+            <ScoreContainer className='ui' />
+
+            <h2 className="ui dividing header">Links</h2>
+            <a href="https://loja.reativo.com/" target="_blank">Acesso a loja</a>
+          </div>
+
+          <div className="column six wide">
+            <h2 className="ui dividing header">Auditoria</h2>
+            <TransactionsContainer className='ui' />
+
+            <h2 className="ui dividing header">Moderação</h2>
+            <ModerationContainer className='ui' />
+          </div>
+        </div>
       </div>
     );
   }
