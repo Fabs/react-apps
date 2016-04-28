a1274cef38c318d890f2c560d8ddf137c6a54846
Fabricio Nascimento <fabriciosn@gmail.com>
2016-04-28 11:52:18 -0300

    Fix expending points method

--- board/imports/reducers/players.js
+++ board/imports/reducers/players.js
@@ -2,9 +2,9 @@ const players = (state = [], action) => {
   switch(action.type) {
       case 'FLUSH_PLAYERS':
         let players = action.players.map((player) => {
-          let coffee = (player.legacy_points || 0) + (player.coffee || 0) - (player.used || 0);
+          let coffee = (player.legacy_points || 0) + (player.coffee || 0);
           let total =  coffee + (player.bread || 0);
-          let balance = total - (player.mess || 0);
+          let balance = total - (player.mess || 0) + (player.used || 0);
           let joke = (player.joke || 0);
           return Object.assign({} ,player ,{ total, coffee, balance });
         });
